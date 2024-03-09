0b3c06d21c8b651685ece03c43a4e5a504bda972
Roosa Villa <roosa001@live.com>
2024-03-09 14:34:06 +0200

    nappien korjausta + reseptin asettelua

--- src/Etusivu.js
+++ src/Etusivu.js
@@ -1,38 +1,78 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./Etusivu.css";
 import { VscSearch } from "react-icons/vsc";
+import db_reseptit from "./db_reseptit.json";
 
 const Etusivu = ({ menuOpen }) => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const [showLeftButton, setShowLeftButton] = useState(false);
   const [showRightButton, setShowRightButton] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState(null); // Tilamuuttuja hakutulokselle
+  const [searchPerformed, setSearchPerformed] = useState(false); // Tilamuuttuja, joka kertoo, onko haku suoritettu
+
   const circleContainerRef = useRef(null);
 
   useEffect(() => {
     const updateButtons = () => {
       const container = circleContainerRef.current;
       if (container) {
-        const scrollableWidth = container.scrollWidth - container.clientWidth;
+        const scrollableWidth =
+          container.scrollWidth - container.clientWidth;
         setShowLeftButton(container.scrollLeft > 0); // Näytä vasen nappula, jos scrolli on vasemmalla
         setShowRightButton(container.scrollLeft < scrollableWidth); // Näytä oikea nappula, jos scrolli on oikealla
       }
     };
-  
+
     // Päivitä napit myös näyttölaitteen leveyden muutoksissa
     const handleResize = () => {
       updateButtons();
     };
-  
+
     // Lisää tapahtumankäsittelijä näytön koon muutoksille
-    window.addEventListener('resize', handleResize);
-  
+    window.addEventListener("resize", handleResize);
+
     // Ensimmäinen päivitys
     updateButtons();
-  
+
     // Poista tapahtumankäsittelijä kun komponentti puretaan
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [scrollPosition]);
-  
+
+  const handleSearch = () => {
+    const lowercaseQuery = searchQuery.toLowerCase().trim();
+
+    // Suodatetaan reseptit hakusanan perusteella
+    const foundRecipes = db_reseptit.reseptit.filter((recipe) => {
+      const lowercaseName = recipe.nimi.toLowerCase().replace(/\s+/g, "");
+      return lowercaseName.includes(lowercaseQuery);
+    });
+
+    // Aseta hakutulos tilaan
+    setSearchResult(foundRecipes);
+
+    // Aseta tila hakutuloksen perusteella
+    setSearchPerformed(true);
+
+    if (foundRecipes.length !== 0) {
+      console.log(foundRecipes); // Tulosta löytyneet reseptit konsoliin
+    } else {
+      console.log("Ei löytynyt reseptejä");
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    // Aseta hakutulos nulliksi ja tila, että haku on suoritettu, kun käyttäjä muuttaa hakusanaa
+    setSearchResult(null);
+    setSearchPerformed(false);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
 
   const scrollRight = () => {
     const container = circleContainerRef.current;
@@ -73,7 +113,19 @@ const Etusivu = ({ menuOpen }) => {
             <input
               type="text"
               placeholder="Hae reseptejä"
-              style={{padding:"15px" }}
+              style={{
+                padding: "15px",
+                outline: "none",
+              }}
+              value={searchQuery}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+              onFocus={(e) => {
+                e.target.style.border = "2px solid rgba(110, 73, 47, 1)";
+              }} // Kun input-kenttä saa fokuksen, muutetaan reunus ruskeaksi
+              onBlur={(e) => {
+                e.target.style.border = "none";
+              }} // Kun input-kenttä menettää fokuksen
             />
             <div
               style={{
@@ -81,7 +133,9 @@ const Etusivu = ({ menuOpen }) => {
                 top: "50%",
                 right: "10px",
                 transform: "translateY(-50%)",
+                cursor: "pointer",
               }}
+              onClick={handleSearch}
             >
               <VscSearch />
             </div>
@@ -89,29 +143,60 @@ const Etusivu = ({ menuOpen }) => {
         </div>
       </div>
       <div className="reseptit-container">
-        <h3>UUSIMMAT RESEPTIT</h3>
+        <h3>
+          {searchPerformed
+            ? searchResult && searchResult.length > 0
+              ? `HAULLASI LÖYTYI ${searchResult.length} RESEPTI${
+                  searchResult.length > 1 ? "Ä" : ""
+                }`
+              : "HAULLASI EI LÖYTYNYT RESEPTEJÄ"
+            : "UUSIMMAT RESEPTIT"}
+        </h3>
         <div className="circle-container" ref={circleContainerRef}>
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="circle-with-text">
-              <div className="circle"></div>
-              <div className="circle-text">Ruuan nimi</div>
-            </div>
-          ))}
+          {searchPerformed && searchResult ? (
+            searchResult.map((recipe, index) => (
+              <div key={index} className="circle-with-text">
+                <div
+                  className="circle"
+                  style={{
+                    backgroundImage: `url(${require(`./${recipe.kuva}`)})`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+                <div className="circle-text">{recipe.nimi}</div>
+              </div>
+            ))
+          ) : (
+            [...Array(6)].map((_, index) => (
+              <div key={index} className="circle-with-text">
+                <div className="circle"></div>
+                <div className="circle-text">Ruuan nimi</div>
+              </div>
+            ))
+          )}
         </div>
         <div className="scroll-buttons">
-          <button
-            onClick={scrollLeft}
-            style={{ visibility: showLeftButton ? "visible" : "hidden" }}
-            disabled={scrollPosition === 0}
-          >
-            {"<"}
-          </button>
-          <button
-            onClick={scrollRight}
-            style={{ visibility: showRightButton ? "visible" : "hidden" }}
-          >
-            {">"}
-          </button>
+          {searchPerformed ? null : (
+            <>
+              <button
+                onClick={scrollLeft}
+                style={{
+                  visibility: showLeftButton ? "visible" : "hidden",
+                }}
+                disabled={scrollPosition === 0}
+              >
+                {"<"}
+              </button>
+              <button
+                onClick={scrollRight}
+                style={{
+                  visibility: showRightButton ? "visible" : "hidden",
+                }}
+              >
+                {">"}
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -119,3 +204,4 @@ const Etusivu = ({ menuOpen }) => {
 };
 
 export default Etusivu;
+
