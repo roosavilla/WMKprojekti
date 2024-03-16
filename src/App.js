362d1d90656e390ed4c94547382495deca3997b4
Roosa Villa <roosa001@live.com>
2024-03-16 16:55:46 +0200

    reitiystä vaihdettu taaaas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Etusivu from "./Etusivu";
 import Kategoriat from "./Kategoriat";
 import Uutiset from "./Uutiset";
